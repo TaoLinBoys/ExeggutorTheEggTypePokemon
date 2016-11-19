0e11673993835c1d06b16e454900684a73400692
Tao Lin <lin.tao.5711y@gmail.com>
2016-11-19 12:50:37 -0500

    progress

--- Execute.c
+++ Execute.c
@@ -1,12 +1,29 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
-void main(){
-  char string[60];
+#include <stdlib.h>
+int main(){
+
+  //taking in input
+  char string[10];
   printf("insert command: ");
   fgets(string,sizeof(string),stdin);
-  printf("%s" , string);
-  strsep(&string," ");
-  
-  
+
+  //replaces \n with null
+  string[strlen(string)-1] = 0;
+
+  //splitting input
+  char *commandArr[100];
+  char *input = string;
+  int ctr = 0;
+  while(input){
+    commandArr[ctr] = strsep(&input," ");
+    ctr++;
+  }
+  //commandArr[ctr] = 0;
+
+  //running it
+  execvp(commandArr[0], commandArr);
+
+  return 0;
 }
